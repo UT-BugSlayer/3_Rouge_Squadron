8f830904cd7eb8d254df0585c2ffd9813a051f67
larodriguez3 <42559788+larodriguez3@users.noreply.github.com>
2020-10-12 17:35:02 -0500

    Init Upload

---
+++ Assets/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")][SerializeField] float speed = 4f; // 4 meters/sec
+    [Tooltip("In m")][SerializeField] float xRange = 6f; // 6 meters
+    [Tooltip("In m")][SerializeField] float yRange = 3f; // 3 meters
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation() {
+        // Process Horizonal Translation
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        // Process Vertical Translation
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        // Set position of Rocket Ship
+        transform.localPosition =
+            new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
+    }
+
+    private void ProcessRotation() {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
---
+++ Steps/README.txt
@@ -0,0 +1,3 @@
+If you duplicate a level the terrain wil be linked for the two levels. Any changes in one will appear in thhe other.
+Create a new terrain and make sure you attach this new terrain in the Inspector > Terrain Collider > Terrain Data field.
+Drag your new terrain to this field.
