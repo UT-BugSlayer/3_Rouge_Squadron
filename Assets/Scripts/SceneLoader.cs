7e5327b4b8b085405ce917dcf76510ebdaa5df06
larodriguez3 <42559788+larodriguez3@users.noreply.github.com>
2021-02-08 19:05:09 -0600

    Tweaks
    -Cleaned up code
    - made prefabs

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    [SerializeField] float levelLoadDelay = 2f;
+    // [SerializeField] float levelLoadDelay = 15f;  //for final build (music beat drop)
+    // Start is called before the first frame update
+    void Start() {
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
+
+    void LoadNextScene() {
+        SceneManager.LoadScene(1);
+    }
+}
