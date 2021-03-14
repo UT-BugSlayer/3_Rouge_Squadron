29dbe368334cc75257daba7d97e74d7189eca97b
larodriguez3 <42559788+larodriguez3@users.noreply.github.com>
2021-03-13 20:11:25 -0600

    Created SelfDestruct to free up resources

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+    [SerializeField] float timeTillDestroy = 3f;
+    void Start() {
+        Destroy(gameObject, timeTillDestroy);
+    }
+
+}
