60abf93c61034d64b6c0b9a9dcc3a23f3879843a
larodriguez3 <42559788+larodriguez3@users.noreply.github.com>
2021-03-13 23:52:11 -0600

    29. Public Methods
    Created ScoreBoard using public method

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
+
+    void Start() {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     void OnParticleCollision(GameObject other) {
+        processHit();
+        killEnemy();
+    }
+
+    private void processHit() {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+    
+    private void killEnemy() {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
     }
+
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour {
+    int score;
+
+    public void IncreaseScore(int amountToIncrease){
+        score += amountToIncrease;
+        print("Score: "+ score);
+    }
+
+
+}
