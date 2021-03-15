5e81d90f79d497bff7f9c6f266024f8b519d45de
larodriguez3 <42559788+larodriguez3@users.noreply.github.com>
2021-03-15 01:36:56 -0500

    32. Enemy Hit Points
    Gave enemies health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 10;
+    [SerializeField] int health = 3;
 
     ScoreBoard scoreBoard;
 
     void Start() {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        AddRigidbody();
+    }
+
+    void AddRigidbody() {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void OnParticleCollision(GameObject other) {
         processHit();
-        killEnemy();
+
+        if (health < 1) {
+            killEnemy();
+        }
     }
 
     private void processHit() {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         scoreBoard.IncreaseScore(scorePerHit);
+        health--;
     }
-    
+
     private void killEnemy() {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
