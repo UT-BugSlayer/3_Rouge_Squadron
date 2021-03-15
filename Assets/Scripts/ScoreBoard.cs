4f18a8acbd2fda0f4c217bc4978e34891ab7b80f
larodriguez3 <42559788+larodriguez3@users.noreply.github.com>
2021-03-15 00:22:18 -0500

    31. ToString() to display score
    Scoreboard is up and running now

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour {
     int score;
+    TMP_Text scoreText;
+
+    void Start(){
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int amountToIncrease){
         score += amountToIncrease;
-        print("Score: "+ score);
+        scoreText.text = score.ToString();
     }
 
 
